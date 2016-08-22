8a0a96164fddca1dd5824c5897fa8adf7f6a59ea
at-phuocnt2 <nguyenthanhphuoc0503@gmail.com>
2016-08-22 15:09:29 +0700

    try login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,12 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'title_login' => 'Login Sharing Food',
+    'btn_login' => 'LOGIN',
+    'btn_register' => 'REGISTER',
+    'remember' => 'Remember Me',
+    'forget' => 'Forget Password',
+    'or_sign_in' => 'Or sign in with &nbsp;',
+    'or_sign_up' => 'Or sign up with &nbsp;',
+    'accept' => 'I accept terms and conditions',
 ];
